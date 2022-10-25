5784fc623d430bb34040da6855756c0b2e7488fb
Sabrina Keeler <sabrina.s.keeler@gmail.com>
2022-10-25 10:50:44 -0400

    add components

--- what-s-for-dinner/src/app/app.module.ts
+++ what-s-for-dinner/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { RatingComponent } from './rating/rating.component';
+import { PreferencesComponent } from './preferences/preferences.component';
+import { RandomizerComponent } from './randomizer/randomizer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    FavoritesComponent,
+    RatingComponent,
+    PreferencesComponent,
+    RandomizerComponent
   ],
   imports: [
     BrowserModule
---
+++ what-s-for-dinner/src/app/randomizer/randomizer.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RandomizerComponent } from './randomizer.component';
+
+describe('RandomizerComponent', () => {
+  let component: RandomizerComponent;
+  let fixture: ComponentFixture<RandomizerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RandomizerComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(RandomizerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
