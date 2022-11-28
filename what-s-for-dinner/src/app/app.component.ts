27b0c232b925d7a9c882e344a24bb9a356f57cd5
Paul Kuhn <pauledwardkuhn@gmail.com>
2022-11-28 17:10:26 -0500

    added links to nav bar, properties to component.ts

--- what-s-for-dinner/src/app/app.component.ts
+++ what-s-for-dinner/src/app/app.component.ts
@@ -6,18 +6,30 @@ import { Router } from '@angular/router';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
 })
 export class AppComponent {
   title = "What's for Dinner?";
 
   constructor(private router: Router) { }
+  
 
 
   navigate(destination: string)
   {
     this.router.navigateByUrl(destination);
   }
-
+export class dayOfweek {
+  constructor(
+    public Monday: string,
+    public Tuesday: string,
+    public Wednesday: string,
+    public Thursday: string,
+    public Friday: string,
+    public Saturday: string,
+    public Sunday: string
+  ) { }
+}
 }
 
 
