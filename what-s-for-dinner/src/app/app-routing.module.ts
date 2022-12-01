4931ddabc19b5ffd5614177b2d3a483031bb791c
Sabrina Keeler <sabrina.s.keeler@gmail.com>
2022-12-01 09:48:12 -0500

    links favorites list to favorite link

--- what-s-for-dinner/src/app/app-routing.module.ts
+++ what-s-for-dinner/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { WeekDisplayComponent } from './week-display/week-display.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { PreferencesComponent } from './preferences/preferences.component';
+import { FavoritesListComponent } from './favorites-list/favorites-list.component'
 // import { RecipeManagerComponent } from './recipe-manager/recipe-manager.component';
 
 const routes: Routes = [
@@ -15,6 +16,7 @@ const routes: Routes = [
   { path: 'detail/:name', component: RecipeDetailComponent },
   { path: 'shopping-list', component: ShoppingListComponent },
   { path: 'preferences', component: PreferencesComponent },
+  { path: 'favorites-list', component: FavoritesListComponent}
 
 ];
 
