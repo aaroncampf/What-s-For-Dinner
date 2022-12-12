30dcbdfc9e4e41b19ebe570008d1fbf0052f61ed
Rafael <rafycruz23@live.com>
2022-12-11 23:25:40 -0500

    added the pdf download to FAV and Recipe

--- what-s-for-dinner/src/app/favorites-list/favorites-list.component.ts
+++ what-s-for-dinner/src/app/favorites-list/favorites-list.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 import { Recipe } from '../models/recipe.model';
 // import { FavoritesComponent } from '../favorites/favorites.component';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-favorites-list',
@@ -28,12 +30,26 @@ export class FavoritesListComponent implements OnInit {
 
       }
     }
-
-
-  
-
 }
-  ngOnInit(): void {
+
+public exportPDF() {
+   let doc = new jsPDF('p','pt','letter');
+   let margin = 10;
+   let scale = (doc.internal.pageSize.width - margin *2) / document.body.scrollWidth;
+   doc.html(document.body, {
+    x: margin,
+    y: margin,
+    html2canvas: {
+      scale: scale,
+    },
+    callback:function(doc){
+      doc.save('recipe.pdf')
+    }
+   });
+    
+}
+  
+ngOnInit(): void {
   }
 
 }
